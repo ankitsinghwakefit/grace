e1db166e44f42feb85dedd82eab38f589d8ee5ea
Ankit Singh <ankitsinghwakefit@gmail.com>
2020-12-06 09:16:37 +0530

    intro made responsive

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="introContainer">
+  <div>
+    <b-container>
+      <b-row>
+        <b-col xl="6" lg="6" md="6" sm="12">
+          <div>
+            <img class="logoresize" src="~/assets/MuktiLogo.jpg" alt="Logo" />
+        </div>
+          </b-col
+        >
+        <b-col xl="6" lg="6" md="6" sm="12">
+            <div>
+            <ul>
+             <li>
+                 Work From <span class="home">Home</span> Himachal.
+             </li>
+             <li>
+                 Sit by the window when it rains & read book.
+             </li>
+             <li>
+                 Listen to yourself.
+             </li>
+             <li>
+                 Fall asleep when the moon is high & wake up slowly, with no place to rush off to.
+             </li>
+             <li>
+                 Just be boundless & infinite.
+             </li>
+         </ul>
+         </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+  <!-- <div class="introContainer">
     <div class="introChild">
      <div class="intrologobox">
             <img class="logoresize" src="~/assets/MuktiLogo.jpg" alt="Kitten" />
@@ -24,13 +57,13 @@
          </ul>
      </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
 
-.introContainer {
+/* .introContainer {
   width: 100%;
   height: 20vh;
   margin: 40px 0;
@@ -94,6 +127,25 @@ li {
 }
 .logoresize {
     width: 350px;
+}
+} */
+.home {
+    text-decoration: line-through;
+    color: brown;
+}
+.logoresize {
+    width: 350px;
+}
+ul {
+    margin-top: 50px;
+}
+li {
+    font-size: 20px;
+    color: #373445;
+}
+@media only screen and (max-width: 750px) { 
+   ul {
+    margin-top: 1px;
 }
 }
 </style>
