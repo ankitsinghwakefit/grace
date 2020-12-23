456c51e4c84f88f02c51c90e551015a5b6574ac7
Ankit Singh <ankitsinghwakefit@gmail.com>
2020-12-23 19:22:43 +0530

    form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,26 +1,6 @@
 <template>
-<!-- <div>
   <div>
-    <b-form-group
-      id="fieldset-horizontal"
-      label-cols-sm
-      label-cols-md="2"
-      label-cols-lg="1"
-      label-cols-xl="1"
-      
-      content-cols-lg="1"
-      content-cols-md="1"
-      content-cols-xl="1"
-      description="Let us know your name."
-      label="Enter your name"
-      label-for="input-horizontal"
-    >
-      <b-form-input id="input-horizontal"></b-form-input>
-    </b-form-group>
-  </div>
-</div> -->
-<div>
-<div v-if="showForm">
+    <!-- <div v-if="showForm">
   <div class="testing">
     <b-form-group
       id="fieldset-1"
@@ -30,6 +10,7 @@
       valid-feedback="Thank you!"
       :invalid-feedback="invalidFeedback"
       :state="state"
+      v-model="name"
     >
       <b-form-input
         id="input-1"
@@ -49,7 +30,7 @@
     >
       <b-form-input
         id="input-2"
-        v-model="digits"
+        v-model="number"
         :state="dig"
         trim
       ></b-form-input>
@@ -57,88 +38,162 @@
     </b-form-group>
     <b-form-textarea
       id="textarea"
-      
+      v-model="msg"
       placeholder="Your Message..."
       rows="3"
       max-rows="6"
     ></b-form-textarea>
     <div class="tetsig4">
+      
         <b-button  class="submitBtn" type="submit" ><a target="_blank" href="https://forms.gle/TWodrXiE7uF5crmWA">Submit</a></b-button>
     </div>
     
   </div>
-  </div>
-  <div class="errorMsg">
-    {{msg}}
-  </div>
+  </div> -->
+    <div>
+      <b-form @submit="onSubmit">
+        <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.name"
+            type="text"
+            placeholder="Enter Name"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          label="Your Number:"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="form.number"
+            type="number"
+            placeholder="Enter Number"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-3"
+          label="Your Msg:"
+          label-for="textarea"
+        >
+        <b-form-textarea
+        
+          id="textarea"
+          v-model="form.msg"
+          placeholder="Enter your Msg..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea></b-form-group>
+
+        <b-button class="mt-4" type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </div>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
 export default {
-  methods: {
-    displayForm(){
-      this.showForm = false;
-      this.msg = "* Your network is not secure, Please try with safe connection or Call on above number"
-    }
-  },
-  computed: {
-    state() {
-      return this.name.length >= 4;
-    },
-    dig() {
-      return this.digits.length >= 9;
-    },
-    invalidFeedback() {
-      if (this.name.length > 0) {
-        return "Enter at least 4 characters.";
-      }
-      return "Please enter something.";
-    },
-    invalidDigits() {
-      if (this.digits.length > 0) {
-        return "Enter at least 10 digits.";
-      }
-      return "Please enter something.";
-    }
-  },
   data() {
     return {
-      name: "",
-      digits: "",
-      showForm : true,
-      msg: "",
+      form: {
+        name: "",
+        number: "",
+        msg: ""
+      }
     };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+      console.log(this.form);
+      axios
+        .post("https://muktiv2.herokuapp.com/", {
+          name: this.form.name,
+          number: this.form.number,
+          msg: this.form.msg
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
   }
 };
+
+// computed: {
+//   state() {
+//     return this.name.length >= 4;
+//   },
+//   dig() {
+//     return this.number.length >= 9;
+//   },
+//   invalidFeedback() {
+//     if (this.name.length > 0) {
+//       return "Enter at least 4 characters.";
+//     }
+//     return "Please enter something.";
+//   },
+//   invalidDigits() {
+//     if (this.number.length > 0) {
+//       return "Enter at least 10 digits.";
+//     }
+//     return "Please enter something.";
+//   }
+// },
+// data() {
+//   return {
+//     name: "",
+//     number: "",
+//     msg: ""
+//   };
+// },
+// methods: {
+//   displayForm() {
+//     this.showForm = false;
+//     this.msg =
+//       "* Your network is not secure, Please try with safe connection or Call on above number";
+//   },
+//   formSubmitted() {
+//     console.log("testing", name);
+//   }
+// }
 </script>
 
 <style scoped>
 .testing {
-    width: 100%;
-    border: 1px solid #ab9856;
-    border-radius: 8px;
-    padding: 8px;
+  width: 100%;
+  border: 1px solid #ab9856;
+  border-radius: 8px;
+  padding: 8px;
 }
 @media only screen and (max-width: 1000px) {
-    .testing {
+  .testing {
     width: 100%;
+  }
 }
- }
- @media only screen and (max-width: 800px) {
-    .testing {
+@media only screen and (max-width: 800px) {
+  .testing {
     width: 100%;
+  }
 }
- }
- @media only screen and (max-width: 750px) {
-    .testing {
+@media only screen and (max-width: 750px) {
+  .testing {
     width: 100%;
+  }
 }
- }
- .button {
+.button {
   display: inline-block;
   border-radius: 13px;
-  background-color: #AB9856;
+  background-color: #ab9856;
   border: none;
   color: #373445;
   text-align: center;
@@ -158,11 +213,11 @@ export default {
   transition: 0.5s;
 }
 a {
-    color: #373445;
+  color: #373445;
 }
 
 .button span:after {
-  content: '\00bb';
+  content: "\00bb";
   position: absolute;
   opacity: 0;
   top: 0;
@@ -179,21 +234,22 @@ a {
   right: 0;
 }
 button:hover {
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
 .tetsig4 {
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    margin: 13px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin: 13px;
 }
 .submitBtn {
-    width: 160px;
-    background-color: #AB9856;
-    border-color: #AB9856;
-    font-weight: bolder;
-    color: #373445;
+  width: 160px;
+  background-color: #ab9856;
+  border-color: #ab9856;
+  font-weight: bolder;
+  color: #373445;
 }
 .errorMsg {
   color: rgb(204, 52, 52);
