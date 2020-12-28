337b64268d47b0436c34af1e94b608c95663879e
Ankit Singh <ankitsinghwakefit@gmail.com>
2020-12-28 19:20:13 +0530

    features

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,9 +1,6 @@
 <template>
-
   <div class="featurecontainer">
-   
     <b-container class="">
-      
       <b-row>
         <div class="featureheading">
           Features
@@ -11,8 +8,30 @@
       </b-row>
 
       <b-row>
-        <b-col xl="6" lg="6" md="6" sm="12">
-          <div class="mt-3">
+        <b-col xl="6" lg="6" md="12" sm="12" class="mt-4">
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="~assets/fea1.jpg"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Location">
+                  <b-card-text>
+                    <b-badge variant="danger"
+                        ><i class="fab fa-periscope"></i></b-badge>
+                    <span class="home">CITY</span>, a small Village - Kangra
+                    (Away from cities) in Heart of Dharamshala - Himachal
+                    Pradesh.
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+          <!-- <div class="mt-3">
             <b-card-group deck class="mb-3">
               <b-card
                 border-variant="dark"
@@ -25,10 +44,31 @@
                 </b-card-text>
               </b-card>
             </b-card-group>
-          </div>
+          </div> -->
         </b-col>
-        <b-col xl="6" lg="6" md="6" sm="12">
-          <div class="mt-3">
+        <b-col xl="6" lg="6" md="12" sm="12" class="mt-4">
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="~assets/location.jpg"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Pick up drop off service">
+                  <b-card-text>
+                    <b-badge variant="success"
+                        ><i class="fas fa-taxi"></i></b-badge>
+                    Airport/Bus stand pick up or drop off service to avoid any
+                    Inconvenience.
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+          <!-- <div class="mt-3">
             <b-card-group deck class="mb-3">
               <b-card
                 border-variant="dark"
@@ -36,11 +76,12 @@
                 class="text-center"
               >
                 <b-card-text
-                  >Airport/Bus stand pick up or drop off service to avoid any Inconvenience.</b-card-text
+                  >Airport/Bus stand pick up or drop off service to avoid any
+                  Inconvenience.</b-card-text
                 >
               </b-card>
             </b-card-group>
-          </div>
+          </div> -->
         </b-col>
       </b-row>
       <!-- <b-row>
@@ -79,8 +120,46 @@
         </b-col>
       </b-row> -->
       <b-row>
-        <b-col xl="6" lg="6" md="6" sm="12">
-          <div class="mt-3">
+        <b-col xl="6" lg="6" md="12" sm="12" class="mt-4">
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="~assets/parag.jpg"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Fun on demand">
+                  <b-card-text>
+                    <span class="fun"
+                      ><b-badge variant="success"
+                        ><i class="fab fa-pied-piper-square"></i
+                      ></b-badge>
+                      Paragliding</span
+                    >
+                    <br />
+                    <span class="fun"
+                      ><b-badge variant=""
+                        ><i class="fas fa-mountain"></i
+                      ></b-badge>
+                      Trekking</span
+                    >
+                    <br />
+                    <span class="fun"
+                      ><b-badge variant="danger"
+                        ><i class="fas fa-campground"></i
+                      ></b-badge>
+                      Camping</span
+                    >
+                    <br />
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+          <!-- <div class="mt-3">
             <b-card-group deck class="mb-3">
               <b-card
                 border-variant="dark"
@@ -88,30 +167,67 @@
                 class="text-center"
               >
                 <b-card-text
-                  ><span class="fun">Paragliding</span> - Over high mountains in the region.<br>
-                  <span class="fun">Trekking</span> - Himachal is a playground for trekkers.<br>
-                  <span class="fun">Camping</span> - The best way to enjoy the wonders of nature.<br>
-                  
+                  ><span class="fun">Paragliding</span> - Over high mountains in
+                  the region.<br />
+                  <span class="fun">Trekking</span> - Himachal is a playground
+                  for trekkers.<br />
+                  <span class="fun">Camping</span> - The best way to enjoy the
+                  wonders of nature.<br />
                 </b-card-text>
               </b-card>
             </b-card-group>
-          </div>
+          </div> -->
         </b-col>
-        <b-col xl="6" lg="6" md="6" sm="12">
-          <div class="mt-3">
+        <b-col xl="6" lg="6" md="12" sm="12" class="mt-4">
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="~assets/acom.jpg"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Accommodation">
+                  <b-card-text>
+                    <b-badge variant="success"
+                      ><i class="fas fa-wifi"></i
+                    ></b-badge>
+                    Hi-Speed Internet.<br />
+                    <b-badge variant="danger"
+                      ><i class="fas fa-home"></i
+                    ></b-badge>
+                    Comfortable Stay.<br />
+                    <b-badge variant="secondary"
+                      ><i class="fas fa-laptop-house"></i
+                    ></b-badge>
+                    WFH Setup.<br />
+                    <b-badge variant="danger"
+                      ><i class="fas fa-mug-hot"></i
+                    ></b-badge>
+                    Tea, Breakfast, Lunch & Dinner.
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+          <!-- <div class="mt-3">
             <b-card-group deck class="mb-3">
               <b-card
                 border-variant="dark"
                 header="Accommodation"
                 class="text-center"
               >
-                <b-card-text>High-Speed Internet.<br>
-                Comfortable Stay.<br>
-               Table - Chair & Work Desks to WFH(Himachal).<br>
-                Tea, Breakfast, Lunch & Dinner.<br></b-card-text>
+                <b-card-text
+                  >High-Speed Internet.<br />
+                  Comfortable Stay.<br />
+                  Table - Chair & Work Desks to WFH(Himachal).<br />
+                  Tea, Breakfast, Lunch & Dinner.<br
+                /></b-card-text>
               </b-card>
             </b-card-group>
-          </div>
+          </div> -->
         </b-col>
       </b-row>
     </b-container>
@@ -119,7 +235,7 @@
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Andika+New+Basic:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Andika+New+Basic:ital,wght@0,400;0,700;1,400;1,700&display=swap");
 .card-text {
   color: #373445;
   font-size: 20px;
@@ -134,15 +250,15 @@
   font-weight: bolder;
 }
 .card-header {
-  background-color: #658EA9;
+  background-color: #658ea9;
   color: #373445;
   font-size: 25px;
   font-weight: bolder;
 }
 .featurecontainer {
-   background-color: #E7D4C0;
+  background-color: #e7d4c0;
   padding-bottom: 50px;
-  font-family: 'Andika New Basic', sans-serif;
+  font-family: "Andika New Basic", sans-serif;
 }
 .featureheading {
   color: #373445;
